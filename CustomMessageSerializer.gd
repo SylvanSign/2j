cfe499b3f3b636c47b3dc9f1b0d2bec9cf6c00c6
SylvanSign <jake.d.bray@gmail.com>
2022-03-20 14:58:34 -0400

    wip on sg phys

--- CustomMessageSerializer.gd
+++ CustomMessageSerializer.gd
@@ -7,7 +7,6 @@ const input_path_mapping := {
 
 enum HeaderFlags {
 	HAS_INPUT_VECTOR = 1 << 0, # Bit 0
-	DROP_BOMB        = 1 << 1, # Bit 1
 }
 
 var input_path_mapping_reverse := {}
@@ -19,29 +18,27 @@ func _init() -> void:
 func serialize_input(all_input: Dictionary) -> PoolByteArray:
 	var buffer := StreamPeerBuffer.new()
 	buffer.resize(16)
-	
+
 	buffer.put_u32(all_input['$'])
 	buffer.put_u8(all_input.size() - 1)
 	for path in all_input:
 		if path == '$':
 			continue
 		buffer.put_u8(input_path_mapping[path])
-		
+
 		var header := 0
-		
+
 		var input = all_input[path]
 		if input.has('input_vector'):
 			header |= HeaderFlags.HAS_INPUT_VECTOR
-		if input.get('drop_bomb', false):
-			header |= HeaderFlags.DROP_BOMB
-		
+
 		buffer.put_u8(header)
-		
+
 		if input.has('input_vector'):
 			var input_vector: Vector2 = input['input_vector']
 			buffer.put_float(input_vector.x)
 			buffer.put_float(input_vector.y)
-	
+
 	buffer.resize(buffer.get_position())
 	return buffer.data_array
 
@@ -49,23 +46,21 @@ func unserialize_input(serialized: PoolByteArray) -> Dictionary:
 	var buffer := StreamPeerBuffer.new()
 	buffer.put_data(serialized)
 	buffer.seek(0)
-	
+
 	var all_input := {}
-	
+
 	all_input['$'] = buffer.get_u32()
-	
+
 	var input_count = buffer.get_u8()
 	if input_count == 0:
 		return all_input
-	
+
 	var path = input_path_mapping_reverse[buffer.get_u8()]
 	var input := {}
-	
+
 	var header = buffer.get_u8()
 	if header & HeaderFlags.HAS_INPUT_VECTOR:
 		input["input_vector"] = Vector2(buffer.get_float(), buffer.get_float())
-	if header & HeaderFlags.DROP_BOMB:
-		input["drop_bomb"] = true
-	
+
 	all_input[path] = input
 	return all_input
--- Player.gd
+++ Player.gd
@@ -1,6 +1,4 @@
-extends Node2D
-
-const Bomb = preload("res://Bomb.tscn")
+extends SGKinematicBody2D
 
 var input_prefix := "player1_"
 
@@ -8,19 +6,16 @@ var speed := 0.0
 
 func _get_local_input() -> Dictionary:
 	var input_vector = Input.get_vector(input_prefix + "left", input_prefix + "right", input_prefix + "up", input_prefix + "down")
-	
+
 	var input := {}
 	if input_vector != Vector2.ZERO:
 		input["input_vector"] = input_vector
-	if Input.is_action_just_pressed(input_prefix + "bomb"):
-		input["drop_bomb"] = true
-	
+
 	return input
 
-func _predict_remote_input(previous_input: Dictionary, ticks_since_real_input: int) -> Dictionary:
-	var input = previous_input.duplicate()
-	input.erase("drop_bomb")
-	return input
+#func _predict_remote_input(previous_input: Dictionary, ticks_since_real_input: int) -> Dictionary:
+#	var input = previous_input.duplicate()
+#	return input
 
 func _network_process(input: Dictionary) -> void:
 	var input_vector = input.get("input_vector", Vector2.ZERO)
@@ -30,9 +25,6 @@ func _network_process(input: Dictionary) -> void:
 		position += input_vector * speed
 	else:
 		speed = 0.0
-	
-	if input.get("drop_bomb", false):
-		SyncManager.spawn("Bomb", get_parent(), Bomb, { position = global_position })
 
 func _save_state() -> Dictionary:
 	return {
